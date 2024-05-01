66fac61cbe2bf892653b65b7347d88d9c43d9782
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-05-01 19:07:38 +0300

    again btn implemented

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
@@ -9,10 +9,13 @@ document.querySelector(".check").addEventListener("click", function () {
 
   // Game reset
   document.querySelector(".again").addEventListener("click", function () {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
     document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").textContent = "?";
     document.querySelector(".number").style.width = "15rem";
     document.querySelector(".message").textContent = "Start guessing...";
     document.querySelector(".score").textContent = 20;
+    document.querySelector(".guess").value = "";
   });
 
   // When there is no input
